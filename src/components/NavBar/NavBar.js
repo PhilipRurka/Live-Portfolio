e60ece8726e7f2a3233e537919e2e9e506bf6bb6
Philip Rurka <philiprurka@gmail.com>
2019-10-17 10:49:04 -0400

    Completed the NavItem Section. Its working as expected!

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { jsx, keyframes } from "@emotion/core";
 import styled from '@emotion/styled/macro';
 import { COLORS } from '../../helpers/colors';
-import './styles.scss';
 
 class Navbar extends React.Component {
 
@@ -39,110 +38,91 @@ class Navbar extends React.Component {
   };
 };
 
-const selectedAnimation = keyframes`
-  0% {
-    width: 0;
-  }
-
-  100% {
-    width: 100%;
-  }
-`;
-
 class NavItem extends React.Component {
 
-  activeState = null;
-
-  contentStyle = {
-    position: 'relative',
-    display: 'inline-block',
-    padding: '1.25em 2em',
-
-    '&::before, &::after': {
-      content: '""',
-      position: 'absolute',
-      left: '0',
-      width: '100%',
-      height: '0.3rem',
-      backgroundColor: COLORS.green
-    },
-
-    '&::before': {
-      top: 0
-    },
-
-    '&::after': {
-      bottom: 0
-    }
-  }
-
-  labelStyle = (triggeredLocation) => {
+  Content = styled.div(({ triggeredLocation }) => {
     const { props: { currentLocation } } = this;
-
-    let beforeStyle;
+    let psudoStyle;
     let addedStyle;
-    const animation = `${selectedAnimation} 1s ease-in-out`;
+
+    const sharedStyles = {
+      overflow: 'hidden',
+      width: '0',
+      transition: 'width 0.3s ease'
+    };
 
     if(currentLocation === triggeredLocation) {
       this.activeState = true;
       addedStyle = null;
-      beforeStyle = { animation };
+      psudoStyle = { width: '100%' };
 
     } else {
-      if(this.activeState) {
-        beforeStyle = {
-          animation,
-          animationDirection: 'reverse',
-          width: '0'
-        };
-
-      } else {
-        beforeStyle = {
-          width: '0',
-        };
-      };
-
+      psudoStyle = { width: '0' };
       addedStyle = { cursor: 'pointer' };
       this.activeState = false;
     };
 
     return {
       position: 'relative',
-      margin: '0',
-      color: COLORS.red,
-      letterSpacing: '1px',
-      ...addedStyle,
-
-      // '&:hover::after': {
-      //   width: '100%'
-      // },
-
+      display: 'inline-block',
+      padding: '1.25em 2em',
+  
       '&::before, &::after': {
-        content: 'attr(title)',
+        content: '""',
         position: 'absolute',
-        top: '0',
         left: '0',
-        overflow: 'hidden',
-        transition: 'width 0.5s ease',
+        width: '100%',
+        height: '0.3rem',
+        backgroundColor: COLORS.green,
+        width: '0',
+        ...sharedStyles,
+        ...psudoStyle
       },
-
+  
       '&::before': {
-        color: COLORS.green,
-        zIndex: '1',
-        ...beforeStyle
+        top: 0
+      },
+  
+      '&::after': {
+        bottom: 0
       },
 
-      '&::after': {
-        color: COLORS.purple,
-        width: '0'
-      },
+
+      'label': {
+        position: 'relative',
+        margin: '0',
+        color: COLORS.red,
+        letterSpacing: '1px',
+        ...addedStyle,
+  
+        '&:hover::after': {
+          width: '100%'
+        },
+  
+        '&::before, &::after': {
+          content: 'attr(title)',
+          position: 'absolute',
+          top: '0',
+          left: '0',
+          ...sharedStyles
+        },
+  
+        '&::before': {
+          color: COLORS.green,
+          zIndex: '1',
+          ...psudoStyle
+        },
+  
+        '&::after': {
+          color: COLORS.purple,
+        },
+      }
     };
-  };
+  });
 
   render() {
     const {
-      contentStyle,
-      labelStyle,
+      Content,
       props: {
         triggeredLocation,
         copy,
@@ -151,15 +131,14 @@ class NavItem extends React.Component {
     } = this;
 
     return (
-      <div
-        css={contentStyle}
+      <Content
+        triggeredLocation={triggeredLocation}
       >
         <label
           title={copy}
           onClick={() => ( goToPage(triggeredLocation) )}
-          css={ labelStyle(triggeredLocation) }
         >{copy}</label>
-      </div>
+      </Content>
     );
   };
 };
