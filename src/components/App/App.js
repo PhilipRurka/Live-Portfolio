67ec52dd7f2aef0aa240826bd7441f215b7a96a9
Philip Rurka <philiprurka@gmail.com>
2019-10-22 09:01:28 -0400

    Triggering Deployment!

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,9 @@ import styled from '@emotion/styled/macro';
 import Header from '../Header';
 import Router from '../Router';
 
-const GlobalWrapper = styled.div({});
+const GlobalWrapper = styled.div({
+  
+});
 
 const App = () => (
   <GlobalWrapper>
